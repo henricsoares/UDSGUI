012c9ef8120c8a179f0e6dddafbc9cdd0213be76
henricsoares <56280902+henricsoares@users.noreply.github.com>
2020-12-10 08:03:33 -0300

    log

--- udsGui.py
+++ udsGui.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox  # noqa: F401
-from uds import Uds
-import string
+from uds import Uds  # noqa: F401
+import string  # noqa: F401
+from time import time
+from threading import Thread
+import tkinter.scrolledtext as tkst
+from datetime import datetime
 
 
 class App(tk.Frame):
@@ -9,19 +13,21 @@ class App(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.winfo_toplevel().title("UDS on Python")
-        self.frame = tk.Frame(window, background="white")
+        self.frame = tk.Frame(window)
         self.frame.pack(fill=tk.BOTH, expand=True)
+        self.tpTime = 0
+        self.tpTimee = 0
 # ------ connection title
         self.connectionTitleMenu = tk.Canvas(self.frame, width=600, height=100,
-                                             background='white',
                                              bd=0, highlightthickness=0)
         self.requestTitleLabel = tk.Label(self.connectionTitleMenu,
-                                          text='Configure the connection:')
+                                          text='Configure the connection',
+                                          relief=tk.RIDGE)
         self.requestTitleLabel.pack()
-        self.connectionTitleMenu.pack(side=tk.TOP)
+        # self.connectionTitleMenu.pack(side=tk.TOP)
+        self.connectionTitleMenu.grid(row=0, column=0)
 # ------ communication menu
         self.communicationMenu = tk.Canvas(self.frame, width=600, height=100,
-                                           background='white',
                                            bd=0, highlightthickness=0)
         self.reqIdLabel = tk.Label(self.communicationMenu, text='Request ID:')
         self.reqId = tk.Entry(self.communicationMenu, bd=5, width=5)
@@ -49,45 +55,59 @@ class App(tk.Frame):
                                            self.device,
                                            self.devices[0])
         self.deviceOptions.config(state='disabled')
-        self.bdLabel = tk.Label(self.communicationMenu, text='Baudrate:')
+        self.sensor = tk.StringVar(window)
+        self.sensors = ('SGU', 'BEG')
+        self.sensor.set(self.sensors[0])
+        self.sensorLabel = tk.Label(self.communicationMenu,
+                                    text='Sensor:')
+        self.sensorOptions = tk.OptionMenu(self.communicationMenu,
+                                           self.sensor,
+                                           self.sensors[0])
+        self.sensorOptions.config(state='disabled')
+        self.bdLabel = tk.Label(self.communicationMenu, text='Baudrate:',
+                                pady=10)
         self.baudRate = tk.Entry(self.communicationMenu, bd=5, width=6)
         self.baudRate.insert(0, '500000')
         self.baudRate.config(state='disabled')
-        self.reqIdLabel.pack(side=tk.LEFT)
-        self.reqId.pack(side=tk.LEFT)
-        self.resIdLabel.pack(side=tk.LEFT)
-        self.resId.pack(side=tk.LEFT)
-        self.interfaceLabel.pack(side=tk.LEFT)
-        self.interfaceOptions.pack(side=tk.LEFT)
-        self.deviceLabel.pack(side=tk.LEFT)
-        self.deviceOptions.pack(side=tk.LEFT)
-        self.bdLabel.pack(side=tk.LEFT)
-        self.baudRate.pack(side=tk.LEFT)
-        self.communicationMenu.pack(side=tk.TOP)
+
+        self.reqIdLabel.grid(row=2, column=0)
+        self.reqId.grid(row=2, column=1)
+        self.resIdLabel.grid(row=2, column=2)
+        self.resId.grid(row=2, column=3)
+        self.interfaceLabel.grid(row=1, column=0)
+        self.interfaceOptions.grid(row=1, column=1)
+        self.deviceLabel.grid(row=1, column=2)
+        self.deviceOptions.grid(row=1, column=3)
+        self.sensorLabel.grid(row=0, column=0)
+        self.sensorOptions.grid(row=0, column=1)
+        self.bdLabel.grid(row=0, column=2)
+        self.baudRate.grid(row=0, column=3)
+        # self.communicationMenu.pack(side=tk.TOP)
+        self.communicationMenu.grid(row=1, column=0)
 # ------ comm status
-        self.commStatus = tk.Canvas(self.frame, width=600, height=100,
-                                    background='white',
-                                    bd=0, highlightthickness=0)
-        self.commButton = tk.Button(self.commStatus, text="Connect",
+        self.commStatus = False
+        self.commButton = tk.Button(self.communicationMenu, text="Connect",
                                     command=self.configComm)
-        self.commLabel = tk.Label(self.commStatus,
-                                  text='Status: waiting...')
-        self.commLabel.pack(side=tk.TOP)
-        self.commButton.pack(side=tk.TOP)
-        self.commStatus.pack(side=tk.TOP)
+        self.commLabel = tk.Label(self.communicationMenu,
+                                  text='Comm status:')
+        self.commStatuss = tk.Label(self.communicationMenu,
+                                    text=str(self.commStatus),
+                                    pady=10)
+        self.commLabel.grid(row=3, column=2)
+        self.commStatuss.grid(row=3, column=3)
+        self.commButton.grid(row=3, column=1)
 # ------ request title
         self.requestTitleMenu = tk.Canvas(self.frame, width=600, height=100,
-                                          background='white',
                                           bd=0, highlightthickness=0)
         self.requestTitle = tk.Label(self.requestTitleMenu,
-                                     text='Mount the request: ')
+                                     text='Select a service',
+                                     relief=tk.RIDGE)
         self.requestTitle.pack(side=tk.TOP)
-        self.requestTitleMenu.pack(side=tk.TOP)
-# ------ request menu
-        self.requestMenu = tk.Canvas(self.frame, width=600, height=400,
-                                     background='white',
-                                     bd=0, highlightthickness=0)
-        self.services = {
+        # self.requestTitleMenu.pack(side=tk.TOP)
+# ------ request menu for BEG
+        self.begRequestMenu = tk.Canvas(self.frame, width=600, height=400,
+                                        bd=0, highlightthickness=0)
+        self.begServices = {
                          'Diagnostic Session Control': 0x10,
                          'ECU Reset': 0x11,
                          'Comunication Control': 0x28,
@@ -98,7 +118,7 @@ class App(tk.Frame):
                          'Clear Diagnostics Information': 0x14,
                          'Control DTC Setting': 0x85,
                          'Security Access': 0x27}
-        self.dataToRead = {
+        self.begDataToRead = {
             'Read active diagnostic session': 0xF186,
             'Read system supplier identifier': 0xF18A,
             'Read ECU manufacturing date': 0xF18B,
@@ -141,14 +161,14 @@ class App(tk.Frame):
             'Configure enable/disable object/filtered object and/or zone message': 0xFD62,  # noqa: E501
             'Configure radar wave emission stop': 0xFD63,
             'Configure output coordinate system': 0xFD64}
-        self.dataOptions = {'Read Data': self.dataToRead,
+        self.dataOptions = {'Read Data': self.begDataToRead,
                             'Write Data': self.dataToWrite}
 
         self.service = tk.StringVar(window)
         self.service.trace('w', self.updateData)
-        self.serviceLabel = tk.Label(self.requestMenu, text='SID')
-        self.serviceOptions = tk.OptionMenu(self.requestMenu, self.service,
-                                            *self.services.keys())
+        self.serviceLabel = tk.Label(self.begRequestMenu, text='SID')
+        self.serviceOptions = tk.OptionMenu(self.begRequestMenu, self.service,
+                                            *self.begServices.keys())
         # self.serviceOptions.config(state='disabled')
         self.DSCSF = {
                       'Default diagnostic session': 0x01,
@@ -165,25 +185,24 @@ class App(tk.Frame):
             'ECU Reset': self.ECUR,
             'Read DTC Information': self.RDTCI}
         self.sFunction = tk.StringVar(self)
-        self.sFunctionLabel = tk.Label(self.requestMenu, text='SubFn')
-        self.sFunctionOptions = tk.OptionMenu(self.requestMenu, self.sFunction,
-                                              '')
+        self.sFunctionLabel = tk.Label(self.begRequestMenu, text='SubFn')
+        self.sFunctionOptions = tk.OptionMenu(self.begRequestMenu,
+                                              self.sFunction, '')
         self.sFunctionOptions['menu'].delete(0, tk.END)
 
         self.dataIdentifier = tk.StringVar(self)
-        self.dIdentifierLabel = tk.Label(self.requestMenu, text='DID')
-        self.dIdentifierOptions = tk.OptionMenu(self.requestMenu,
+        self.dIdentifierLabel = tk.Label(self.begRequestMenu, text='DID')
+        self.dIdentifierOptions = tk.OptionMenu(self.begRequestMenu,
                                                 self.dataIdentifier, '')
         self.dIdentifierOptions['menu'].delete(0, tk.END)
 
-        self.dRecordLabel = tk.Label(self.requestMenu, text='DataRec')
-        self.dataRecord = tk.Entry(self.requestMenu, bd=5, width=20,
-                                   disabledbackground='silver',
+        self.dRecordLabel = tk.Label(self.begRequestMenu, text='DataRec')
+        self.dataRecord = tk.Entry(self.begRequestMenu, bd=5, width=20,
                                    state=tk.DISABLED)
 
-        self.B3 = tk.Button(self.requestMenu, text="Send",
+        self.B3 = tk.Button(self.begRequestMenu, text="Send",
                             command=self.sendRequest)
-        # self.B3.config(state='disabled')
+        self.B3.config(state='disabled')
         self.serviceLabel.pack(side=tk.LEFT)
         self.serviceOptions.pack(side=tk.LEFT)
         self.sFunctionLabel.pack(side=tk.LEFT)
@@ -193,15 +212,67 @@ class App(tk.Frame):
         self.dRecordLabel.pack(side=tk.LEFT)
         self.dataRecord.pack(side=tk.LEFT)
         self.B3.pack(side=tk.LEFT)
-        self.requestMenu.pack(side=tk.TOP)
+        # self.begRequestMenu.pack(side=tk.TOP)
+# ------ request menu for SGU
+        self.sguDIDS = {
+            'Programming Attempt Counter': [0x22, 0xf1, 0x10],
+            'Boot Software Identification': [0x22, 0xf1, 0x80],
+            'Application Software Identification': [0x22, 0xf1, 0x81],
+            'Active Diagnostic Session': [0x22, 0xf1, 0x86],
+            'ECU Voltage': [0x22, 0xf1, 0x87],
+            'ECU Software Number': [0x22, 0xf1, 0x88],
+            'ECU Manufacturing Date': [0x22, 0xf1, 0x8b],
+            'Serial Number': [0x22, 0xf1, 0x8c],
+            'ECU Hardware Partnumber': [0x22, 0xf1, 0x91],
+            'ECU Hardware Version Information': [0x22, 0xf1, 0x93],
+            'ECU Temperature': [0x22, 0xf1, 0x94],
+            'SDA Horizontal Misalignment Angle': [0x22, 0xfc, 0x01],
+            'SDA Vertical Alignment Angle': [0x22, 0xfc, 0x02],
+            'SDA Status': [0x22, 0xfc, 0x03]}
+        self.sguTPS = {
+            'ON - With response': [0, 0, 0],
+            'ON - Without response': [0, 0, 0],
+            'OFF': [0, 0, 0]}
+        self.sguRequestMenu = tk.Canvas(self.frame, width=600, height=400,
+                                        bd=0, highlightthickness=0)
+        self.sguDID = tk.StringVar(self)
+        self.sguDIDLabel = tk.Label(self.sguRequestMenu,
+                                    text='Read Data By Identifier: ')
+        self.sguDIDOptions = tk.OptionMenu(self.sguRequestMenu, self.sguDID,
+                                           *self.sguDIDS.keys())
+        self.sguTP = tk.StringVar(self)
+        self.sguTP.set('OFF')
+        self.sguTP.trace('w', self.startTrd)
+        self.sguTPLabel = tk.Label(self.sguRequestMenu,
+                                   text='Tester Present: ')
+        self.sguTPOptions = tk.OptionMenu(self.sguRequestMenu, self.sguTP,
+                                          *self.sguTPS.keys())
+        self.sguSend = tk.Button(self.sguRequestMenu, text="Send",
+                                 command=self.sguSend)
+        self.sguResponse = tk.Label(self.sguRequestMenu,
+                                    text='Response: ',
+                                    pady=10)
+        self.sguDIDLabel.grid(row=1, column=0)
+        self.sguDIDOptions.grid(row=1, column=1)
+        self.sguSend.grid(row=2, column=0)
+        self.sguResponse.grid(row=2, column=1)
+        self.sguTPLabel.grid(row=0, column=0)
+        self.sguTPOptions.grid(row=0, column=1)
+        # self.sguRequestMenu.pack(side=tk.TOP)
 # ------ response menu
         self.responseMenu = tk.Canvas(self.frame, width=600, height=100,
-                                      background='white',
                                       bd=0, highlightthickness=0)
         self.response = tk.Label(self.responseMenu,
                                  text='Response: ')
-        self.response.pack(side=tk.TOP)
-        self.responseMenu.pack(side=tk.TOP)
+        # self.response.pack(side=tk.TOP)
+        # self.responseMenu.pack(side=tk.TOP)
+# ------ log menu
+        self.terminalMenu = tk.Canvas(self.frame, width=100, height=50,
+                                      bd=0, highlightthickness=0)
+        self.term = tkst.ScrolledText(master=self.terminalMenu, wrap=tk.WORD,
+                                      width=40,
+                                      height=10)
+        self.term.pack()
 # ------
 
     def updateData(self, *args):
@@ -240,10 +311,10 @@ class App(tk.Frame):
         else:
             if self.service.get() == 'Read Data':
                 if self.dataIdentifier.get() != '':
-                    message = (hex(self.services.get(self.service.get(), '')),
-                               # self.sFunctions.get(self.sFunction.get(), ''),
-                               hex(self.dataToRead.get(self.dataIdentifier.get(),  # noqa: E501
-                                                       '')))
+                    message = (hex(self.begServices.get(self.service.get(),
+                                   '')),
+                               hex(self.begDataToRead.get(self.dataIdentifier.get(),  # noqa: E501
+                                                          '')))
                     if tk.messagebox.askyesno('Confirm', "SID: " + self.service.get() + "\n" + "DID: " + self.dataIdentifier.get()):  # noqa: E501
                         self.response['text'] = 'Request: ' + str(message)
                         # print(message)
@@ -253,8 +324,8 @@ class App(tk.Frame):
 
             elif self.service.get() == 'Write Data':
                 if self.dataRecord.get() != '' and self.dataIdentifier.get() != '':  # noqa: E501
-                    message = (hex(self.services.get(self.service.get(), '')),
-                               # hex(self.sFunctions.get(self.sFunction.get(), '')),  # noqa: E501
+                    message = (hex(self.begServices.get(self.service.get(),
+                               '')),
                                hex(self.dataToWrite.get(self.dataIdentifier.get(),  # noqa: E501
                                                         '')),
                                self.dataRecord.get())
@@ -270,7 +341,7 @@ class App(tk.Frame):
                     messagebox.showinfo("Error",
                                         'Data Record is missing')
             elif self.service.get() == 'Diagnostic Session Control' or self.service.get() == 'ECU Reset' or self.service.get() == 'Read DTC Information':   # noqa: E501
-                message = (hex(self.services.get(self.service.get(), '')),
+                message = (hex(self.begServices.get(self.service.get(), '')),
                            hex((self.sfOptions.get(self.service.get())).get(self.sFunction.get())))  # noqa: E501
                 if tk.messagebox.askyesno('Confirm the content',
                                           "SID: " + self.service.get() + "\n" + "SubFn: " + self.sFunction.get()):  # noqa: E501
@@ -280,7 +351,7 @@ class App(tk.Frame):
                 if tk.messagebox.askyesno('Confirm the content',
                                           self.service.get()):
                     # self.response['text'] = 'Request: ' + str(message)
-                    msg = self.a.send(self.services.get(self.service.get()))
+                    msg = self.a.send(self.begServices.get(self.service.get()))
                     '''if msg[0] == 0x7e:
                         msg[1] = 'Positive response '
                     else:
@@ -288,7 +359,7 @@ class App(tk.Frame):
                     self.response['text'] = 'Response: ' + str(msg)
 
     def configComm(self):
-        reqId, resId = self.reqId.get(), self.resId.get()
+        '''reqId, resId = self.reqId.get(), self.resId.get()
         interface, device = self.interface.get(), self.device.get()
         baudrate = self.baudRate.get()
         aux = True
@@ -326,7 +397,46 @@ class App(tk.Frame):
                 self.B3.config(state='disabled')
                 messagebox.showinfo('Error', 'There is no connection')
         else:
-            messagebox.showinfo('Error', msg)
+            messagebox.showinfo('Error', msg)'''
+        self.commStatus = not self.commStatus
+        self.commStatuss.config(text=str(self.commStatus))
+        if not self.commStatus:
+            self.commButton.config(text='Connect')
+            self.requestTitleMenu.grid_forget()
+            self.sguRequestMenu.grid_forget()
+            self.terminalMenu.grid_forget()
+        else:
+            self.commButton.config(text='Disconnect')
+            # self.requestTitleMenu.pack(side=tk.TOP)
+            # self.sguRequestMenu.pack(side=tk.TOP)
+            self.requestTitleMenu.grid(row=2, column=0)
+            self.sguRequestMenu.grid(row=3, column=0)
+            self.terminalMenu.grid(row=4, column=0)
+            self.termPrint('connected')
+
+    def sguSend(self):
+        self.termPrint(self.sguDID.get())
+
+    def testerPresent(self, *args):
+        self.tpTimee += 4
+        while self.commStatus and self.sguTP.get() == 'ON - With response' or self.sguTP.get() == 'ON - Without response':  # noqa: E501
+            if self.tpTimee - self.tpTime >= 4:
+                self.tpTime, self.tpTimee = time(), time()
+                self.termPrint(self.sguTP.get())
+            else:
+                self.tpTimee = time()
+
+    def termPrint(self, info):
+        now = datetime.now()
+        now = now.strftime('%m/%d/%Y, %H:%M:%S')
+        self.term.config(state=tk.NORMAL)
+        self.term.insert('insert',
+                         now + ': ' + '\n' + info + '\n')
+        self.term.see("end")
+        self.term.config(state=tk.DISABLED)
+
+    def startTrd(self, *args):
+        Thread(target=self.testerPresent, daemon=True).start()
 
 
 window = tk.Tk()
